3a89b568976ad66fc3a884a6112cd3ae389c3d0a
nizkiyVorobey <formobileprima@gmail.com>
2022-12-15 17:36:19 +0200

    request permission for writing settings is no more needed

--- app/src/main/java/com/senya/novuswidget/MainActivity.kt
+++ app/src/main/java/com/senya/novuswidget/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.senya.novuswidget
 
 import android.content.Intent
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
-import android.provider.Settings.SettingNotFoundException
-import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.getValue
@@ -18,69 +13,37 @@ import com.senya.novuswidget.ui.home.HomeViewModel
 import com.senya.novuswidget.util.DEFAULT_INITIAL_CARD_POSITION
 import com.senya.novuswidget.util.PRESSED_WIDGET_ITEM_INDEX
 
-const val MAX_BRIGHTNESS = 255
 
 class MainActivity : AppCompatActivity() {
-    private var oldBrightness: Int? = null
     var initialListPosition by mutableStateOf(DEFAULT_INITIAL_CARD_POSITION)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initialListPosition = intent?.getIntExtra(PRESSED_WIDGET_ITEM_INDEX, DEFAULT_INITIAL_CARD_POSITION) ?: DEFAULT_INITIAL_CARD_POSITION
+        initialListPosition =
+            intent?.getIntExtra(PRESSED_WIDGET_ITEM_INDEX, DEFAULT_INITIAL_CARD_POSITION)
+                ?: DEFAULT_INITIAL_CARD_POSITION
         setContent {
             val vm: HomeViewModel = viewModel()
             val state = vm.state
             Home(state = state, onAction = vm::onAction, initialListPosition = initialListPosition)
         }
 
-        try {
-            oldBrightness =
-                Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS)
-        } catch (e: SettingNotFoundException) {
-            Log.e("Error", "Cannot access system brightness")
-            e.printStackTrace()
-        }
+        setMaxBrightness()
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        val newInitialListPosition = intent?.getIntExtra(PRESSED_WIDGET_ITEM_INDEX, DEFAULT_INITIAL_CARD_POSITION) ?: DEFAULT_INITIAL_CARD_POSITION
+        val newInitialListPosition =
+            intent?.getIntExtra(PRESSED_WIDGET_ITEM_INDEX, DEFAULT_INITIAL_CARD_POSITION)
+                ?: DEFAULT_INITIAL_CARD_POSITION
         initialListPosition = newInitialListPosition
     }
 
-    override fun onStop() {
-        super.onStop()
-        if (checkWriteSettingsPermission()) {
-            oldBrightness?.let { setBrightness(it) }
-        }
+
+    private fun setMaxBrightness() {
+        val layoutParams = window.attributes
+        layoutParams.screenBrightness = 1f
+        window.attributes = layoutParams
     }
-
-    override fun onResume() {
-        super.onResume()
-        if (!checkWriteSettingsPermission()) {
-            openAndroidPermissionsMenu()
-        } else {
-            setBrightness(MAX_BRIGHTNESS)
-        }
-    }
-
-    private fun checkWriteSettingsPermission(): Boolean {
-        var canWrite = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            canWrite = Settings.System.canWrite(this)
-        }
-        return canWrite
-    }
-
-    private fun openAndroidPermissionsMenu() {
-        val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        intent.data = Uri.parse("package:" + this.packageName)
-        startActivity(intent)
-    }
-
-
-    private fun setBrightness(value: Int) {
-        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, value); }
 
 }
